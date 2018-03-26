eaf3cd60520ef4be99ea911124d2bc52618b7c28
Yu-Jin <yujinost@gmail.com>
2018-03-26 17:32:09 +0100

    first commit

--- RemovingDuplicates/Program.cs
+++ RemovingDuplicates/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿/* 
+"Write a function that given a list of countries, returns the country(s) 
+that occur the least times. 
+i.e - [""Argentina"", ""Cuba"", ""Chile"", ""Argentina""] -> [""Cuba"", ""Chile""]"                      
+*/
+using System;
+using System.Collections.Generic;
 
-namespace RemovingDuplicates
+public class Test
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        List<string> output = new List<string>();
+        string[] countries = new string[] { "Argentina", "Cuba", "Chile", "Argentina" };
+
+        Console.WriteLine("List Before: ");
+        for (int i = 0; i < countries.Length; i++)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(countries[i]);
+            if (output.Contains(countries[i]))
+            {
+                //get index of first occurence and delete it
+                output.RemoveAt(output.LastIndexOf(countries[i]));
+            }
+            else
+            {
+                output.Add(countries[i]);
+            }
         }
+
+        Console.WriteLine("List After: ");
+        foreach (string item in output)
+            Console.WriteLine(item);
+        
     }
+
 }
